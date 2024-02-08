43631eb187d64c885973eb467a45389770d7c853
DaniilAliev <de.aliev@yandex.ru>
2024-02-09 00:05:19 +0300

    fix: fixing style by lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 // import Image from "next/image";
 // import { Inter } from "next/font/google";
-
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div></div>
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
   );
 }
