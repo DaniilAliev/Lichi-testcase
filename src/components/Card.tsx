62f1a6283811c8a1c3fddb776ae3d3f63685d8f2
DaniilAliev <de.aliev@yandex.ru>
2024-02-09 18:11:24 +0300

    feat: add hover to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,14 +2,14 @@ import { FC } from "react";
 import CommentForm from "./CommentForm";
 import { Item } from "@/types/types";
 
-const Card: FC<Item> = ({ item }) => {
+const Card: FC<{ item: RestoredItem }> = ({ item }) => {
   return (
-    <div className="max-w-screen-lg h-10 size-full h-full border-2 border-stone-200 rounded-2xl my-3 p-5">
+    <div className="max-w-screen-lg h-10 size-full h-full border-2 border-stone-200 rounded-2xl my-3 p-5 transition-colors duration-200 hover:bg-stone-400 hover:text-white">
       <h2 className="text-3xl mb-4">{item.title}</h2>
-      <p className="text-gray-400 line-clamp-2 mb-4">{item.body}</p>
-      {/* <CommentForm /> */}
-    </div>
-  )
+      <p className="line-clamp-2 mb-4">
+        {item.body}
+      </p>
+    </div>)
 }
 
 export default Card;
