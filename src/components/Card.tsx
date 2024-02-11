28df70b8334d1f7df359e65fe9545c5538023eb2
DaniilAliev <de.aliev@yandex.ru>
2024-02-11 12:02:09 +0300

    feat: add framer motion to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { RestoredItem } from '@/types/types.ts';
 import { useDispatch } from 'react-redux';
 import { actions as modalActions } from '@/slices/modalSlice.ts';
+import { motion } from 'framer-motion';
 import CommentsContainer from './CommentsContainer.tsx';
 
 const Card: FC<{ item: RestoredItem }> = ({ item }) => {
@@ -14,10 +15,18 @@ const Card: FC<{ item: RestoredItem }> = ({ item }) => {
       title, body, postId: id,
     };
 
-    dispatch(modalActions.openModal(dataToState));
+    setTimeout(() => {
+      dispatch(modalActions.openModal(dataToState));
+    }, 100);
   };
 
   return (
+    <motion.div
+      className="box"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+    >
     <div
       className="max-w-screen-lg h-10 size-full h-full border-2 border-stone-200 rounded-2xl my-3 px-5 pt-5 transition-colors duration-200 hover:bg-stone-400 hover:text-white cursor-pointer"
       onClick={handleClick}
@@ -27,7 +36,9 @@ const Card: FC<{ item: RestoredItem }> = ({ item }) => {
         {item.body}
       </p>
       <CommentsContainer id={id} type="card" />
-    </div>);
+    </div>
+    </motion.div>
+  );
 };
 
 export default Card;
