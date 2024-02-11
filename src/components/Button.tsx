731ab7180f4abe203f6a69a3ea4bf63d55540419
DaniilAliev <de.aliev@yandex.ru>
2024-02-11 13:21:16 +0300

    fix: close button style when isEdit = true

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,17 @@
 import { actions as modalActions } from '@/slices/modalSlice.ts';
 import { ButtonType } from '@/types/types.ts';
-import classNames from 'classnames';
 import { FC, memo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectors as editSelectors } from '@/slices/editSlice.ts';
+import classNames from 'classnames';
 
-const getButton = (type: 'post' | 'submit' | 'close'): string => {
-  const mapping = {
-    post: 'Post',
-    submit: 'Submit',
-    close: 'Close',
-  };
-
-  return mapping[type];
+const mapping = {
+  post: 'Post',
+  submit: 'Submit',
+  close: 'Close',
 };
+
+const getButtonText = (type: 'post' | 'submit' | 'close'): string => mapping[type];
 
 const Button: FC<ButtonType> = ({ type }) => {
   const { isEdit } = useSelector(editSelectors);
@@ -21,9 +19,9 @@ const Button: FC<ButtonType> = ({ type }) => {
   const btnClass = classNames('mt-4', 'py-2', 'px-4', 'rounded-2xl', 'text-white', 'transition duration-300', {
     'bg-stone-400': type !== 'close',
     'hover:bg-stone-500': type !== 'close',
-    'bg-gray-400': type === 'close',
-    'hover:bg-gray-500': type === 'close',
-    'bg-gray-100': type === 'close' && isEdit === true,
+    'bg-gray-400': type === 'close' && !isEdit,
+    'hover:bg-gray-500': type === 'close' && !isEdit,
+    'bg-gray-100': type === 'close' && isEdit,
   });
 
   const dispatch = useDispatch();
@@ -41,7 +39,7 @@ const Button: FC<ButtonType> = ({ type }) => {
       onClick={handleClick}
       disabled={type === 'close' && isEdit === true}
     >
-      {getButton(type)}
+      {getButtonText(type)}
     </button>
   );
 };
